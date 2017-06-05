43947e8dab0df04d1e510d1d13c404890b935f4f
James Cleworth <jamescleworth@jamess-mbp.home>
2017-06-05 19:05:13 +0100

    Gruntfile commit

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+grunt.initConfig({
+    'template': {
+        'options': {
+            // Task-specific options go here
+        },
+        'dist': {
+            'options': {
+                // Target-specific options go here
+            },
+            'files': {
+                // Target-specific file lists go here
+            }
+        }
+    }
+});
